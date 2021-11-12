a09006babd07c45b232574be02b3cabf9229da01
unknown <axel.viselthier@student-cs.fr>
2021-11-12 16:54:46 +0100

    Actu branche

--- bazar/brouillon.py
+++ bazar/brouillon.py
@@ -1,7 +1,7 @@
 
 import pygame as pg
 from pygame.locals import *
-
+"""
 def affiche(largeur,hauteur,e,color): #e=largeur de la bande présente en haut et en bas de la map , color=color des bodures en RGB
     pg.init()    
     fenetre = pg.display.set_mode((largeur,hauteur))
@@ -12,6 +12,22 @@ def affiche(largeur,hauteur,e,color): #e=largeur de la bande présente en haut e
     fenetre.blit(ship,(largeur/2,hauteur/2))
     pg.draw.rect(fenetre,color, pg.Rect(0,0,largeur,e))
     pg.draw.rect(fenetre,color, pg.Rect(0,hauteur-e,largeur,e))
+    pg.display.flip()
+    a=1
+    while a:
+        a=int(input())
+"""
+def affiche(largeur,hauteur,e,color): #e=largeur de la bande présente en bas de la map , color=color des bodures en RGB
+    pg.init()    
+    fenetre = pg.display.set_mode((largeur,hauteur))
+    fond=pg.image.load("bazar/etoiles.jpg").convert()
+    fond=pg.transform.scale(fond,(largeur,hauteur))    
+    fenetre.blit(fond,(0,0))
+    ship=pg.image.load("bazar/ship_simple.png").convert()
+    fenetre.blit(ship,(largeur/2,hauteur/2))
+    foreground=pg.image.load("bazar/foreground.png").convert()
+    foreground=pg.transform.scale(foreground,(largeur,e))    
+    fenetre.blit(foreground,(0,hauteur-e))
     pg.display.flip()
     a=1
     while a:
