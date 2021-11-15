af6cba1341f12354784911e1926f7a963ece7b8a
Gauthier Debuisschert <gauthier.debuisschert@student-cs.fr>
2021-11-15 14:23:57 +0100

    fonction detect_collision()

--- game/game.py
+++ game/game.py
@@ -4,9 +4,9 @@ from pygame.locals import *
 
 def detect_collision(ship, L):
     for i in range(len(L)):
-        if ship.colliderect(L[i]):
-            mask_ship = pg.mask.from_surface(ship)
-            mask_enemy = pg.mask.from_surface(L[i])
-            if mask_ship.overlap(mask_enemy, (L[i].left - ship.left, L[i].top - ship.top)) != None:
+        if ship.rect.colliderect(L[i].rect):
+            mask_ship = pg.mask.from_surface(ship.rect)
+            mask_enemy = pg.mask.from_surface(L[i].rect)
+            if mask_ship.overlap(mask_enemy, (L[i].rect.left - ship.rect.left, L[i].rect.top - ship.rect.top)) != None:
                 return True
     return False
