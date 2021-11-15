1dd018283b31a63fe01d703d222e65550e5908fa
Paul Creusy <paul.creusy@student-cs.fr>
2021-11-15 11:57:28 +0100

    Ajout des images et bug fix

--- game/constant.py
+++ game/constant.py
@@ -32,7 +32,7 @@ volume_musique = 0.2
 volume_bruitage = 0.5
 
 # Création du dictionnaire pour importer les images
-dico_image = {"vaisseau": "resources/images/ship_simple.jpeg"}
+dico_image = {"vaisseau": "resources/images/vaisseau.png"}
 
 
 def chargement_image(dico):
