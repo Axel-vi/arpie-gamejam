58a0bb28205b8208d6e1dd2a96c2d75c73f2648e
Paul Creusy <paul.creusy@student-cs.fr>
2021-11-18 19:58:39 +0100

    opti

--- r_type.py
+++ r_type.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 from game.ship import Vaisseau, l_tir_vaisseau
 from game.game import gestion_event, detect_collision
-from game.graphics import afficher_ecran_demarrage, afficher_ecran_victoire, afficher_niveau_en_cours, detect_control_demarrage, fenetre,\
+from game.graphics import afficher_ecran_demarrage, afficher_ecran_victoire, \
+    afficher_niveau_en_cours, detect_control_demarrage, fenetre,\
     defilement_decor_background, detect_control_game, afficher_vaisseau, \
     afficher_et_update_enemy, afficher_et_update_tir, afficher_et_update_explosion, \
     defilement_decor_foreground, afficher_ecran_fin, initialiser_decor, l_explosion
@@ -49,16 +50,16 @@ while True:
         afficher_et_update_enemy(SHIP)
         afficher_et_update_tir()
         afficher_et_update_explosion()
+        afficher_niveau_en_cours(id_niveau)
         destroy_old_enemy()
         abs_decor = defilement_decor_foreground()
         if detect_collision(SHIP, l_enemy, l_tir_enemy,
                             l_tir_vaisseau, l_missile_enemy, l_tir_tower, abs_decor):
             son_game_over()
             STATE = 2
-        pg.display.update()
-            if COMPTEUR > 60*liste_niveau[id_niveau-1][1] :
+            if COMPTEUR > 60*liste_niveau[id_niveau-1][1]:
                 STATE = 4
-            pg.display.update()
+        pg.display.update()
 
     while STATE == 2:
         # Etat de Game over
@@ -71,32 +72,31 @@ while True:
         pg.display.update()
 
     if STATE == 3:
-            # Initialisation du niveau
-            niveau = [el for el in liste_niveau[id_niveau-1][2]]
-            COMPTEUR = 0
-            niveau_1 = lire_niveau(id_niveau)
-            initialiser_decor()
-            SHIP = Vaisseau()
-            while len(l_enemy) != 0:
-                l_enemy.pop()
-            while len(l_tir_enemy) != 0:
-                l_tir_enemy.pop()
-            while len(l_tir_vaisseau) != 0:
-                l_tir_vaisseau.pop()
-            while len(l_missile_enemy) != 0:
-                l_missile_enemy.pop()
-            while len(l_explosion) != 0:
-                l_explosion.pop()
-            while len(l_tir_tower) != 0:
-                l_tir_tower.pop()
-            STATE = 1
-            musique_jeu()
-    
-    while STATE == 4 :
-        afficher_ecran_victoire() 
+        # Initialisation du niveau
+        niveau = [el for el in liste_niveau[id_niveau-1][2]]
+        COMPTEUR = 0
+        niveau_1 = lire_niveau(id_niveau)
+        initialiser_decor()
+        SHIP = Vaisseau()
+        while len(l_enemy) != 0:
+            l_enemy.pop()
+        while len(l_tir_enemy) != 0:
+            l_tir_enemy.pop()
+        while len(l_tir_vaisseau) != 0:
+            l_tir_vaisseau.pop()
+        while len(l_missile_enemy) != 0:
+            l_missile_enemy.pop()
+        while len(l_explosion) != 0:
+            l_explosion.pop()
+        while len(l_tir_tower) != 0:
+            l_tir_tower.pop()
+        STATE = 1
+        musique_jeu()
+
+    while STATE == 4:
+        afficher_ecran_victoire()
         new_state = detect_control_demarrage()
-        if new_state == 1 :
-            id_niveau+=1
+        if new_state == 1:
+            id_niveau += 1
             STATE = 3
         pg.display.update()
-
